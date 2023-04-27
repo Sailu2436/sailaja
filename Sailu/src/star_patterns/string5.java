55d3010ae8d5b73c8091ea2d66497154b52c3dea
K A R T H I K <K A R T H I K@KARTHIK>
2023-04-27 20:37:28 +0530

    nthg

---
+++ Sailu/src/star_patterns/string5.java
@@ -0,0 +1,16 @@
+package star_patterns;
+
+public class string5 {
+
+	public static void main(String[] args) {
+		int num=2345;
+		int count=0;
+		while(num<=2345)
+		{
+			count=count+num;
+			num++;
+				
+		}
+		System.out.println(count);
+	}
+}
